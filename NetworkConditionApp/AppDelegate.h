caa7168556151528cdd79ed62ca173bbaa2fb7d6
psithikong <paeng@wl-10-190-211-232.wireless.duke.edu>
2013-10-01 16:37:33 -0400

    Initial Commit

---
+++ NetworkConditionApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetworkConditionApp
+//
+//  Created by paeng on 10/1/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkConditionApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NetworkConditionApp
+//
+//  Created by paeng on 10/1/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
